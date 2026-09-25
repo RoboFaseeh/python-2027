2ffe339955a9a2e161b1513736e0e9299d924322
Robo Faseeh <106868483+RoboFaseeh@users.noreply.github.com>
2026-09-25 20:42:01 +0500

    Add files via upload

---
+++ radius.py
@@ -0,0 +1,5 @@
+CONSTPI = 3.142
+Radius = float(input("Please enter the radius of cylinder"))
+Lenght = float(input("enter the lenght "))
+volume = Radius * Radius * Lenght * CONSTPI
+print ("volume of cylinder is ", volume)
